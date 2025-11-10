8876c2192d5905cad1a14b930f2d06741739e37b
md al sahariyar hossain <mdalsahariyarhossain@gmail.com>
2025-11-10 11:24:40 +0530

    Updated all file

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { setLoginState as setCartLogin } from './cartSlice'
 import { setLoginState as setWishlistLogin } from './wishlistSlice'
 
-const API_URL = 'https://jps-shop-backend.vercel.app/auth'
+const API_URL = 'https://jps-shop-backend.vercel.app/api'
 
 // ✅ Load session
 const savedUser = JSON.parse(sessionStorage.getItem('user'))
